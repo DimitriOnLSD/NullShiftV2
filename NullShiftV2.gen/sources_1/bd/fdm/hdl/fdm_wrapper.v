bbcb78e6971b8191228245ee3580eb41063bfac6
Paulo Sousa <2222031@my.ipleiria.pt>
2026-07-08 21:02:30 +0100

    final commit
    comes with report
    comes with many images of PROJECT1

--- NullShiftV2.gen/sources_1/bd/fdm/hdl/fdm_wrapper.v
+++ NullShiftV2.gen/sources_1/bd/fdm/hdl/fdm_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2025.2 (win64) Build 6299465 Fri Nov 14 19:35:11 GMT 2025
-//Date        : Wed Jul  8 01:55:02 2026
+//Date        : Wed Jul  8 20:22:34 2026
 //Host        : LENOVO-LOQ-15IRX9 running 64-bit major release  (build 9200)
 //Command     : generate_target fdm_wrapper.bd
 //Design      : fdm_wrapper
